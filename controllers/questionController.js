c09a56586e9af74eab5495165cb896f61b716fa6
Sridhara.a <sridhara.a@zibtek.in>
2022-01-16 21:26:18 +0530

    modified

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -9,14 +9,41 @@ const showQuestionList = (req, res) => {
         }
     });
 }
+const getQuestionDetails = (req, res) => {
+    const qId = req.params.id;
+    QuestionModel.findById(qId)
+    .then((result) => {
+        res.json({msg: 'Fetch Data', result})
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+}
 const saveQuestionBank = (req, res) => {
     const question = req.body;
     const newQuestion = new QuestionModel(question);
     newQuestion.save();
     res.json({msg: 'Save Successfully', question})
 }
-
+const updateQuestionBank = (req, res) => {
+    const qId = req.params.id;
+    QuestionModel.findByIdAndUpdate(qId, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update! Question Id was not found!`
+        });
+      } else res.send({ message: "Question was updated successfully.", data });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id
+      });
+    });
+}
 module.exports = {
     showQuestionList,
-    saveQuestionBank
+    getQuestionDetails,
+    saveQuestionBank,
+    updateQuestionBank
 }
--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const questionController = require('../controllers/questionController')
 const router = express.Router();
-const QuestionModel =  require('../models/questions');
 
 router.get('/questionList', questionController.showQuestionList);
+router.get('/question/:id', questionController.getQuestionDetails);
 router.post('/createQuestion', questionController.saveQuestionBank);
+router.put('/updateQuestion/:id', questionController.updateQuestionBank);
 
 module.exports = router;
