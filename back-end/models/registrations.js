8cda0228c5c327918763fe99440df1425964f174
Laiphangbam Sheetal Roy <shtlroy6@gmail.com>
2022-09-14 11:13:32 +0530

    add course model

--- back-end/models/registrations.js
+++ back-end/models/registrations.js
@@ -14,7 +14,24 @@ const RegistrationSchema = new mongoose.Schema({
     },
     enroll_no: {
         type: String
-    }
+    }, 
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:Courses
 });
 const RegistrationModel = mongoose.model("registrations", RegistrationSchema)
 module.exports = RegistrationModel;
+
+// add another model course.js
+const mongoose = require('mongoose')
+const CourseSchema = new mongoose.Schema({
+    courseCode: {
+        type: Number
+    },
+    courseName: {
+        type: String
+    }
+    
+});
+const CourseModel = mongoose.model("Courses", CourseSchema)
+module.exports = CourseModel;
